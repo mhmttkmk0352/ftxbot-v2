30ee608546584a655518ac0a60cfe986f83302cd
Mehmet TOKMAK <33135266+mhmttkmk0352@users.noreply.github.com>
2021-02-12 01:05:51 +0300

    Add files via upload

---
+++ v2/chance.js
@@ -0,0 +1,55 @@
+require("dotenv").config();
+const ftx_rest= require("./ekler/ftx-rest");
+
+
+
+while_process_true = () => {
+    console.log("Already process !");
+}
+
+while_process_false = () => {
+    //Just no process. We should get price
+    data = {};
+
+    ftx_rest.get_price().then( price => {
+        data["price"] = price;
+        if ( process.env.process_type == "LONG" ){
+            data["peak_point"] = price+((price/100)*parseFloat(process.env.percent));
+            console.log( data );
+            ftx_rest.buy(process.env.initial_price, data["peak_point"]).then(bought => {
+                data["process_id"] = bought.id;
+                ftx_rest.get_process(data["process_id"]).then(get_process => {
+                    data["process_size"] = get_process.size;
+                    data["process_price"] = get_process.price;
+                    data["process_top_price"] = data["peak_point"];
+                    
+                    ftx_rest.sell( data["process_size"], data["process_top_price"] )
+                    .then(sell=>{
+                        console.log( {sell} );
+                    })
+                    
+                });
+            });
+        }
+
+    })
+    
+
+}
+
+
+
+App = () => {   
+    var timer = setTimeout(() => {
+        ftx_rest.any_proces_status().then( process_status => {
+            process.env.process_status = process_status;
+            if ( process.env.process_status == 0 ){
+                // if not the process. So we can go activity
+                while_process_false();
+            }
+        })  
+    }, process.env.process_control_time);
+}
+
+
+App();
---
+++ v2/ekler/ftx-rest.js
@@ -0,0 +1,112 @@
+const FTXRest = require('./');
+
+const ftx = new FTXRest({
+  key: '6JJZ3CgzHnu4dfAKfzkrlxK9jtHPrta5gbHhWMNX',
+  secret: 'aO1HFNmokHZ9_X7HoEGXX0ogqlX32DlbVCmk4CI6'
+});
+
+
+const output = {
+    dolar_getir: function(kac_dolarlik, kactan){
+        return kac_dolarlik/kactan;
+    },
+    bakiye_getir: async() => {
+        let pr = new Promise((resolve, reject) => {
+            ftx.request({
+                method: 'GET',
+                path: '/wallet/balances',
+               }).then(r=>{
+                    resolve( r.result[0].total );
+               });
+        });
+        return pr;
+    },
+    buy: async function( kac_dolarlik, kactan){
+        let pr = new Promise((resolve, reject) => {
+
+                ftx.request({
+                    method: 'POST',
+                    path: '/orders',
+                    data: {
+                        "market": process.env.coin_name,
+                        "side": "buy",
+                        "price": kactan,
+                        "type": "limit",
+                        "size": this.dolar_getir(kac_dolarlik, kactan),
+                        "reduceOnly": false,
+                        "ioc": false,
+                        "postOnly": false,
+                        "clientId": null
+                    }
+                }).then(r=>{
+                    resolve(r.result);
+                });
+        });
+
+        return pr;
+    },
+    sell: async function(alinan_coin_adedi, kactan){
+        let pr = new Promise((resolve, reject) => {
+                ftx.request({
+                    method: 'POST',
+                    path: '/orders',
+                    data: {
+                        "market": process.env.coin_name,
+                        "side": "sell",
+                        "price": kactan,
+                        "type": "limit",
+                        "size": alinan_coin_adedi,
+                        "reduceOnly": true,
+                        "ioc": false,
+                        "postOnly": false,
+                        "clientId": null
+                      }
+                }).then(r=>{
+                    resolve(r);
+                });
+        });
+
+        return pr;
+    },
+    get_process: async function( process_id ){
+        let pr = new Promise((resolve, reject) => {
+                ftx.request({
+                    method: 'GET',
+                    path: '/orders/'+process_id,
+                }).then(r=>{
+                    resolve(r.result);
+                });
+        });
+        return pr;
+    },
+    any_proces_status: async function(){
+        let pr = new Promise((resolve, reject) => {
+                ftx.request({
+                    method: 'GET',
+                    path: '/orders?market='+process.env.coin_name
+                }).then(r=>{
+                    if ( r.result.length > 0 ){
+                        resolve(1);
+                    }
+                    else{
+                        resolve(0);
+                    }
+                });
+        });
+        return pr;
+    },
+    get_price: async function(){
+        let pr = new Promise((resolve, reject) => {
+                ftx.request({
+                    method: 'GET',
+                    path: '/markets/'+process.env.coin_name
+                }).then(r=>{
+                   resolve( r.result.price );
+                });
+        });
+        return pr;
+    }    
+}
+
+
+module.exports = output;
