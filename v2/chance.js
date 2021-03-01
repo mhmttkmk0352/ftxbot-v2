639e156168b8dbfc92fcfc9a7472feeb3191483a
Mehmet TOKMAK <33135266+mhmttkmk0352@users.noreply.github.com>
2021-03-02 00:54:38 +0300

    Add files via upload

--- v2/chance.js
+++ v2/chance.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const { clearScreenDown } = require("readline");
 const ftx_rest= require("./ekler/ftx-rest");
 const mail = require("./ekler/mail");
 
@@ -13,10 +14,10 @@ while_process_false = () => {
 
     ftx_rest.get_price().then( price => {
         data["price"] = price;
-        if ( process.env.process_type == "LONG" ){
+        if ( process.argv[2] == "LONG" ){
             data["peak_point"] = price+((price/100)*parseFloat(process.env.percent));
             console.log( data );
-            ftx_rest.buy(process.env.initial_price, data["peak_point"]).then(bought => {
+            ftx_rest.buy(process.env.initial_price, data["peak_point"].toFixed(6), "buy", false).then(bought => {
                 setTimeout(() => {
                     data["process_id"] = bought.id;
                     ftx_rest.get_process(data["process_id"]).then(get_process => {
@@ -25,8 +26,8 @@ while_process_false = () => {
                         data["process_top_price"] = get_process.avgFillPrice+((get_process.avgFillPrice/100)*(process.env.percent));
                         data["process_bottom_price"] = get_process.avgFillPrice-((get_process.avgFillPrice/100)*(process.env.percent));
                                                 
-                        ftx_rest.sell( data["process_size"], data["process_top_price"] ).then(sell=>{
-                            ftx_rest.stop( data["process_size"], data["process_bottom_price"] ).then(stop=>{
+                        ftx_rest.sell( data["process_size"], data["process_top_price"].toFixed(6), "sell", true ).then(sell=>{
+                            ftx_rest.stop( data["process_size"], data["process_bottom_price"].toFixed(6), "sell", true ).then(stop=>{
                                 console.log( {stop} );
                             });
                         });               
@@ -34,6 +35,37 @@ while_process_false = () => {
                 },5000);
             });
         }
+        else if (  process.argv[2] == "SHORT" ){
+            console.log("Short pozisyon açılıyor.. ");
+            data["peak_point"] = price-((price/100)*parseFloat(process.env.percent));
+            console.log( {peak_point: data["peak_point"] } );
+            
+            ftx_rest.sell(process.env.initial_price, data["peak_point"].toFixed(6), "sell", false).then(selled => {
+                
+                setTimeout(() => {
+                    data["process_id"] = selled.result.id;
+                    console.log( {selled} );
+                        data["process_size"] = selled.result.size;
+                        data["process_price"] = data["price"];
+                        data["process_top_price"] = data["process_price"]+((data["process_price"]/100)*(process.env.percent));
+                        data["process_bottom_price"] = data["process_price"]-((data["process_price"]/100)*(process.env.percent));
+
+                        console.log( {data} );
+                                 
+                        
+                        ftx_rest.buy( data["process_size"], data["process_bottom_price"].toFixed(6), "buy", true ).then(sell=>{
+                            console.log("Short buy:");
+                            ftx_rest.stop( data["process_size"], data["process_top_price"].toFixed(6), "buy", true ).then(stop=>{
+                                console.log("Short buy:");
+                                console.log( {stop} );
+                            });
+                        });
+                                     
+                },5000);
+                
+                
+            });
+        } 
     })
 }
 
--- v2/ekler/ftx-rest.js
+++ v2/ekler/ftx-rest.js
@@ -1,10 +1,11 @@
 const FTXRest = require('./');
 
 const ftx = new FTXRest({
-  key: '*****CgzHnu4dfAKfzkrl****************',
-  secret: '*********HZ9_X7HoEGXX0o************'
+  key: 'FnUdDG4lf4F9VapY7fHrEjfoadAwSrB7eLYvgV9H',
+  secret: 'K5iXNXkN4Dhs_uETv1DVQgANc50Oq92a4ZPShmj_',
+  subaccount:"Yedek"
 });
-
+__dirname
 
 const output = {
     dolar_getir: function(kac_dolarlik, kactan){
@@ -21,7 +22,7 @@ const output = {
         });
         return pr;
     },
-    buy: async function( kac_dolarlik, kactan){
+    buy: async function( kac_dolarlik, kactan, process_type, reduceType){
         let pr = new Promise((resolve, reject) => {
 
                 ftx.request({
@@ -29,11 +30,11 @@ const output = {
                     path: '/orders',
                     data: {
                         "market": process.env.coin_name,
-                        "side": "buy",
+                        "side": process_type,
                         "price": kactan,
                         "type": "limit",
                         "size": this.dolar_getir(kac_dolarlik, kactan),
-                        "reduceOnly": false,
+                        "reduceOnly": reduceType,
                         "ioc": false,
                         "postOnly": false,
                         "clientId": null
@@ -45,18 +46,18 @@ const output = {
 
         return pr;
     },
-    sell: async function(alinan_coin_adedi, kactan){
+    sell: async function(alinan_coin_adedi, kactan, process_type, reduceType){
         let pr = new Promise((resolve, reject) => {
                 ftx.request({
                     method: 'POST',
                     path: '/orders',
                     data: {
                         "market": process.env.coin_name,
-                        "side": "sell",
+                        "side": process_type,
                         "price": kactan,
                         "type": "limit",
                         "size": alinan_coin_adedi,
-                        "reduceOnly": true,
+                        "reduceOnly": reduceType,
                         "ioc": false,
                         "postOnly": false,
                         "clientId": null
@@ -68,18 +69,18 @@ const output = {
 
         return pr;
     },
-    stop: async function(alinan_coin_adedi, kactan){
+    stop: async function(alinan_coin_adedi, kactan, process_type, reduceType){
         let pr = new Promise((resolve, reject) => {
                 ftx.request({
                     method: 'POST',
                     path: '/conditional_orders',
                     data: {
                         "market": process.env.coin_name,
-                        "side": "sell",
+                        "side": process_type,
                         "triggerPrice": kactan,
                         "size": alinan_coin_adedi,
                         "type": "stop",
-                        "reduceOnly": true,
+                        "reduceOnly": reduceType,
                       }
                 }).then(r=>{
                     resolve(r);
